5e4a874afab1e23fdd2b5f6f088ee04ab5b9b8a5
Mike Lang <mikelang3000@gmail.com>
2021-12-02 00:29:41 +1100

    task 1-1

---
+++ tasks/1-1.asm
@@ -0,0 +1,94 @@
+
+include "macros.asm"
+include "hram.asm"
+include "longcalc.asm"
+
+
+SECTION "Input", ROMX, BANK[1]
+
+Input:
+include "inputs/1.asm"
+EndInput:
+INPUT_LENGTH = EndInput - Input
+
+
+SECTION "Main", ROM0
+
+
+Main::
+	DeclareSet Count, 2, 0
+
+	; by initializing prev value to $ffff, we ensure first comparision isn't "increases"
+	DeclareSet PrevValue, 2, $ff
+
+IF DEBUG > 0
+	LoadAll EndInput, E,D
+	call U16ToStr
+	call Print
+	call PrintLine
+ENDC
+
+	ld DE, Input
+
+.mainloop
+	; count chars to next newline
+	ld B, 0
+	ld H, D
+	ld L, E
+.seekloop
+	ld A, [DE]
+	inc DE
+	cp "\n"
+	jr z, .seekloopbreak
+	inc B
+	jr .seekloop
+.seekloopbreak
+
+	; now (B, HL) is a single line
+	push DE
+	call StrToU16
+	CrashIfNot nz, "Bad Int"
+	ld H, D
+	ld L, E
+	pop DE
+	; HL = parsed int
+
+	; set c if PrevValue < HL
+	LoadAll PrevValue, C,B
+	LongCP BC, HL
+	jr nc, .nocount
+
+	LoadAll Count, C,B
+	inc BC
+	StoreAll Count, C,B
+
+.nocount
+	StoreAll PrevValue, L,H
+
+IF DEBUG > 0
+	; Print progress
+	push DE
+	call AllocMark
+	push HL
+	call U16ToStr
+	call Print
+	call ResetLine
+	pop HL
+	call AllocFree
+	pop DE
+ENDC
+
+	; Check if we're at EOF
+	LoadAll EndInput, C,B
+	LongCP BC, DE ; set z if EOF
+
+	jr nz, .mainloop
+
+	LoadLiteral "Result: "
+	call Print
+	LoadAll Count, E,D
+	call U16ToStr
+	call Print
+	call PrintLine
+
+	ret
